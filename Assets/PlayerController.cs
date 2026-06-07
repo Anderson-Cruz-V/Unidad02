9f8367ac41463fdfec7e60c7c025c50320350478
Anderson-Cruz-V <213361341+Anderson-Cruz-V@users.noreply.github.com>
2026-06-07 00:06:08 -0400

    Pruebas 01

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 8f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector2.right * move * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
